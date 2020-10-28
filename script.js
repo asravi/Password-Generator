b50f913d563173c0e4246c04481be3d53a889d80
Aditya Seshadri <aditya.seshadri.ra@gmail.com>
2020-10-27 23:33:57 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function generate(){
+    let complexity = document.getElementById("slider").value;
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()+=";
+    let password = "";
+
+    for(var i=0; i<=complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    
+    }
+
+    document.getElementById("display").value = password;
+
+    document.getElementById("LastPasswords").innerHTML += password + "<br />";
+        
+}
